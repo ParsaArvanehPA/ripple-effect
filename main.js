4f9dcf45df15e25b257a217a4f76c16a2772dd13
parsaarvanehpa <parsa.arvaneh@gmail.com>
2022-06-10 23:21:33 +0430

    [ADD] active state

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const btn = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
-btn.addEventListener('mousedown', (e) => {
-   const posX = e.offsetX;
-   const posY = e.offsetY;
+buttons.forEach(btn => {
+   btn.addEventListener('mousedown', (e) => {
+      const posX = e.offsetX;
+      const posY = e.offsetY;
 
-   btn.style.setProperty('--x', `${posX}px`);
-   btn.style.setProperty('--y', `${posY}px`);
+      btn.style.setProperty('--x', `${posX}px`);
+      btn.style.setProperty('--y', `${posY}px`);
 
-   btn.classList.add('pulse');
+      btn.classList.add('pulse');
 
-   btn.addEventListener('animationend', () => {
-       btn.classList.remove('pulse');
-   })
-});
+      btn.addEventListener('animationend', () => {
+         btn.classList.remove('pulse');
+      })
+   });
+})
